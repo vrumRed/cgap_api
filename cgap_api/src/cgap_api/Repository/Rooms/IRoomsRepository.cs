db9670ce1f83c9683fe0a510dc9c70f80c7d3b87
AllexTerashima <allex-terashima@hotmail.com>
2017-05-19 14:22:21 -0300

    IRepository Criados

--- cgap_api/src/cgap_api/Repository/Rooms/IRoomsRepository.cs
+++ cgap_api/src/cgap_api/Repository/Rooms/IRoomsRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cgap_api.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,10 @@ namespace cgap_api.Repository.Rooms
 {
     interface IRoomsRepository
     {
+        void Add(Room item);
+        IEnumerable<Room> GetAll();
+        Room Find(int key);
+        void Remove(int Id);
+        void Update(Room itemToUpdate, Room item);
     }
 }
--- cgap_api/src/cgap_api/Repository/Users/IUsersRepository.cs
+++ cgap_api/src/cgap_api/Repository/Users/IUsersRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cgap_api.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,10 @@ namespace cgap_api.Repository.Users
 {
     interface IUsersRepository
     {
+        void Add(User item);
+        IEnumerable<User> GetAll();
+        User Find(int key);
+        void Remove(int Id);
+        void Update(User itemToUpdate, User item);
     }
 }
